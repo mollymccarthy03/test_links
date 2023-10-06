193fc2ac1af2c72f72f0350f3f79d7bec3d5f55a
Alperen <abakirci@madisoncollege.edu>
2023-10-06 09:01:55 -0500

    Trying some stuff

---
+++ links-action/action.sh
@@ -0,0 +1,9 @@
+urls=$(curl -s https://raw.githubusercontent.com/CannonLock/link_checker_config/main/ignore_urls.json | jq -r 'map("/" + . + "/") | join(",")' | sed 's/ //g')
+
+# Echo the result
+echo "Computed URLs: $urls"
+
+# Ensure GITHUB_ENV is available or set a default
+GITHUB_ENV=${GITHUB_ENV:-./github_env.txt}
+
+echo "URLS=$urls" >> $GITHUB_ENV
